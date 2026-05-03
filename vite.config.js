bda30058611b92d17125c0cf828540136e62f256
KnucK <domrachevsascha@yandex.ru>
2026-05-04 00:14:11 +0300

    Тест3

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [plugin()],
-  base: '/FurryMaiker/',
+  base: '/FurryMeiker/',
   server: {
     port: 57347,
   },
