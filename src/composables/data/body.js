0f616635b449370e7ad642e062187fcdecc14fb5
KnucK <domrachevsascha@yandex.ru>
2026-05-03 23:56:38 +0300

    Тест2

--- src/composables/data/body.js
+++ src/composables/data/body.js
@@ -2,42 +2,42 @@ export const body = [
   {
     id: 1,
     name: 'Кошка Голубая',
-    image: '/src/assets/parts/body/layer4_00.png',
+    image: '/parts/body/layer4_00.png',
     category: 'body',
   },
 
   {
     id: 2,
     name: 'Хз Коричневое',
-    image: '/src/assets/parts/body/layer4_01.png',
+    image: '/parts/body/layer4_01.png',
     category: 'body',
   },
 
   {
     id: 3,
     name: 'Кошка Фиолетовая',
-    image: '/src/assets/parts/body/layer4_02.png',
+    image: '/parts/body/layer4_02.png',
     category: 'body',
   },
 
   {
     id: 4,
     name: 'Кошка Зелёная',
-    image: '/src/assets/parts/body/layer4_03.png',
+    image: '/parts/body/layer4_03.png',
     category: 'body',
   },
 
   {
     id: 5,
     name: 'Кошка Розовая',
-    image: '/src/assets/parts/body/layer4_04.png',
+    image: '/parts/body/layer4_04.png',
     category: 'body',
   },
 
   {
     id: 6,
     name: 'Кошка Белая',
-    image: '/src/assets/parts/body/layer4_05.png',
+    image: '/parts/body/layer4_05.png',
     category: 'body',
   },
 ]
